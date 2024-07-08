2d15ddaf0b17f81a65754d2808b3a2eb893991d2
gabrielsouzaleite <gabriel.s.leite05@gmail.com>
2024-07-08 17:56:30 -0300

    Iniciando projeto de uma api para livraria

---
+++ LibraryApi/Controllers/LibraryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers;
+
+public class LibraryController : MyBaseController
+{
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public IActionResult Create()
+    {
+        return Create();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Update([FromRoute] int id)
+    {
+        return NoContent();
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        return NoContent();
+    }
+
+}
---
+++ LibraryApi/Controllers/MyBaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers;
+
+[Route("library-api/[controller]")]
+[ApiController]
+public abstract class MyBaseController : ControllerBase
+{
+}
